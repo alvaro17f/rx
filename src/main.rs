4bfa6c67068658517ac20829ceb74fdd26af2dab
Alvaro <89421445+alvaro17f@users.noreply.github.com>
2026-05-21 10:04:36 +0200

    chore: remove cfg not test

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,10 @@ mod app;
 mod core;
 mod error;
 
-#[cfg(not(test))]
-use app::init;
-#[cfg(not(test))]
-use std::{env, io::stdout};
-#[cfg(not(test))]
 use app::cli::RealDeps;
+use app::init;
+use std::{env, io::stdout};
 
-#[cfg(not(test))]
 fn main() {
     let args: Vec<String> = env::args().collect();
     let mut out = stdout();
